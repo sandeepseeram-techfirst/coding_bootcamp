8b329234a6e92e45765b47ce5a0c8da6cff87b66
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-01-16 16:28:33 +0530

    updated the file

--- Test_Driven_Development/point_of_sale_app/src/pos/pos.js
+++ Test_Driven_Development/point_of_sale_app/src/pos/pos.js
@@ -1,9 +1,36 @@
-export default class pos {
-    constructor() {
-        
-    }
+import sinon from 'sinon';
+import chai, { expect, assert } from 'chai'
+import sinonChai from 'sinon-chai'
 
-    scanItem(itemId) {
-        
-    }
-}
+import PointOfSale from "./PointOfSale";
+import MockDisplay from '../display/MockDisplay';
+import MockPricingService from '../pricingservice/MockPricingService';
+import InventoryItem from '../pricingservice/InventoryItem';
+
+chai.use(sinonChai);
+
+describe('PointOfSale', () => {
+    afterEach(function () {
+        sinon.restore();
+    });
+
+    it('displays the price of a scanned item to the shopper', () => {
+        //Arrange
+        const testItemId = "abc-123";
+        const expectedPrice = 38.99;
+        let actualPrice = null;
+        const expectedItem = new InventoryItem(testItemId, "Test Item", expectedPrice);
+        const mockDisplay = new MockDisplay();
+        const mockPricingService = new MockPricingService();
+        mockPricingService.setItem(expectedItem);
+
+        const testPointOfSale = new PointOfSale(mockDisplay, mockPricingService);
+
+        //Act
+        testPointOfSale.scanItem(testItemId);
+
+        //Assert
+        actualPrice = mockDisplay.displayedPrice;
+        expect(actualPrice).to.equal(expectedPrice);
+    });
+});
--- Test_Driven_Development/point_of_sale_app/src/pos/pos_test.js
+++ Test_Driven_Development/point_of_sale_app/src/pos/pos_test.js
@@ -3,6 +3,11 @@ import chai, { expect, assert } from 'chai'
 import sinonChai from 'sinon-chai'
 
 import PointOfSale from "./PointOfSale";
+import MockDisplay from '../display/MockDisplay';
+import Display from '../display/Display';
+import MockPricingService from '../pricingservice/MockPricingService';
+import PricingService from '../pricingservice/PricingService';
+import InventoryItem from '../pricingservice/InventoryItem';
 
 chai.use(sinonChai);
 
@@ -13,11 +18,59 @@ describe('PointOfSale', () => {
 
     it('displays the price of a scanned item to the shopper', () => {
         //Arrange
+        const testItemId = "abc-123";
+        const expectedPrice = 38.99;
+        let actualPrice = null;
+        const expectedItem = new InventoryItem(testItemId, "Test Item", expectedPrice);
+        const mockDisplay = new MockDisplay();
+        const mockPricingService = new MockPricingService();
+        mockPricingService.setItem(expectedItem);
+
+        const testPointOfSale = new PointOfSale(mockDisplay, mockPricingService);
 
         //Act
+        testPointOfSale.scanItem(testItemId);
 
         //Assert
-        //You will start your test implementation here, for now fail as not implemented.
-        assert.fail("Test not implemented!");
+        actualPrice = mockDisplay.displayedPrice;
+        expect(actualPrice).to.equal(expectedPrice);
+    });
+
+    it('displays the price of a different scanned item to the shopper', () => {
+        //Arrange
+        const testItemId = "def-456";
+        const expectedPrice = 123.45;
+        let actualPrice = null;
+        const expectedItem = new InventoryItem(testItemId, "A Different Test Item", expectedPrice);
+        const mockDisplay = new MockDisplay();
+        const mockPricingService = new MockPricingService();
+        mockPricingService.setItem(expectedItem);
+
+        const testPointOfSale = new PointOfSale(mockDisplay, mockPricingService);
+
+        //Act
+        testPointOfSale.scanItem(testItemId);
+
+        //Assert
+        actualPrice = mockDisplay.displayedPrice;
+        expect(actualPrice).to.equal(expectedPrice);
+    });
+
+    it('displays the price of a scanned item to the shopper using Sinon mocks', () => {
+        //Arrange
+        const testItemId = "sinon-789";
+        const expectedPrice = 78.90;
+        const expectedItem = new InventoryItem(testItemId, "A Sinon Test Item", expectedPrice);
+        const mockDisplay = sinon.createStubInstance(Display);
+        const mockPricingService = sinon.createStubInstance(PricingService);
+        mockPricingService.getItem.withArgs(testItemId).returns(expectedItem);
+
+        const testPointOfSale = new PointOfSale(mockDisplay, mockPricingService);
+
+        //Act
+        testPointOfSale.scanItem(testItemId);
+
+        //Assert
+        expect(mockDisplay.displaySale).to.have.been.calledWith(expectedPrice);
     });
 });
